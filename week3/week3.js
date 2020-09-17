120ee7c062e9a69beef4096e10ffaacbbc751c4d
Brian Kimbrow <bkimbrow25@gmail.com>
2020-09-16 21:40:05 -0500

    Week3HW

---
+++ week3/week3.js
@@ -0,0 +1,17 @@
+const pizzaTopping = ['Peperoni', 'Ham', 'Cheese', 'Pineapple']
+const crust = ["stuffed", "thick", "thin"];
+const size = ["small", "medium", "large"];
+function greetCustomer(){
+ console.log ('welcome to pizzaTopping we have the best pizza')
+};
+function getPizzaOrder (){
+ console.log (Ham, pineapple, peperoni)
+};
+function getPizzaOrder () {
+  (size, crust, pizzaTopping)
+  return (one ,size, crust, pizzaTopping)
+};
+console.log (getPizzaOrder){};
+function preparePizza('servePizza') {
+return ("Order up! Here's your large thick crust pizza with ham enjoy.")
+};
